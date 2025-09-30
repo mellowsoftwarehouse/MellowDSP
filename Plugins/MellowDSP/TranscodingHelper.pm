a82eb1e6affc231464bb4ad174c6a81166d1d741
mellowsoftwarehouse <mellow@softwarehouse.com>
2025-09-30 20:19:34 +0200

    MellowDSP v2.1.3 - Fix capabilities hash format
    Capabilities must be hash ref not string to avoid FileTypes.pm crash

--- Plugins/MellowDSP/TranscodingHelper.pm
+++ Plugins/MellowDSP/TranscodingHelper.pm
@@ -5,10 +5,8 @@ use warnings;
 use Slim::Utils::Log;
 use Slim::Utils::Prefs;
 use Slim::Player::TranscodingHelper;
-use Plugins::MellowDSP::SOXProcessor;
-use Plugins::MellowDSP::FIRProcessor;
 
-my $log = logger('plugin.mellowdsp.transcoding');
+my $log = logger('plugin.mellowdsp');
 my $prefs = preferences('plugin.mellowdsp');
 
 sub init {
@@ -25,86 +23,31 @@ sub registerConverters {
     
     return unless $prefs->get('enabled');
     
-    my @inputFormats = qw(flc aif alc wav);
-    my $outputFormat = 'wav';
+    my $soxPath = $prefs->get('sox_path') || '/usr/bin/sox';
     
-    foreach my $inputFormat (@inputFormats) {
-        my $profile = "$inputFormat-$outputFormat-mellowdsp-*-*";
+    my @conversions = (
+        ['flc', 'wav'],
+        ['aif', 'wav'],
+        ['alc', 'wav'],
+    );
+    
+    foreach my $conv (@conversions) {
+        my ($from, $to) = @$conv;
+        my $profile = "$from-$to-mellowdsp-*-*";
         
-        my $command = buildCommandTemplate($inputFormat, $outputFormat);
+        # Comando base SOX
+        my $command = "$soxPath -t $from \$FILE\$ -t $to -b 24 -";
         
+        # Registra il comando
         $Slim::Player::TranscodingHelper::commandTable{$profile} = $command;
         
-        $Slim::Player::TranscodingHelper::capabilities{$profile} = "MellowDSP: $inputFormat->$outputFormat";
+        # Capabilities deve essere un hash ref, non una stringa!
+        $Slim::Player::TranscodingHelper::capabilities{$profile} = {
+            'T' => 'F',  # Transcode from File to stream
+        };
         
         $log->info("Registered converter: $profile");
     }
-}
-
-sub buildCommandTemplate {
-    my ($inputFormat, $outputFormat) = @_;
-    
-    my $soxPath = $prefs->get('sox_path') || '/usr/bin/sox';
-    
-    my $command = "$soxPath -t $inputFormat \$FILE\$ -t $outputFormat -b 24 -";
-    
-    $log->debug("Command template: $command");
-    
-    return $command;
-}
-
-sub getCommand {
-    my ($client, $song, $format) = @_;
-    
-    return undef unless $client;
-    return undef unless $prefs->client($client)->get('player_enabled');
-    
-    my $url = $song->currentTrack()->url;
-    my $inputFormat = Slim::Music::Info::contentType($url);
-    
-    my $command = Plugins::MellowDSP::SOXProcessor->buildTranscodeCommand(
-        $client,
-        $inputFormat,
-        'wav',
-        '-',
-        '-'
-    );
-    
-    if ($prefs->client($client)->get('fir_enabled')) {
-        my $leftFIR = Plugins::MellowDSP::FIRProcessor->getFIRPath($client, 'left');
-        my $rightFIR = Plugins::MellowDSP::FIRProcessor->getFIRPath($client, 'right');
-        
-        if ($leftFIR || $rightFIR) {
-            $command = addFIRProcessing($command, $leftFIR, $rightFIR);
-        }
-    }
-    
-    $log->info("Transcoding command: $command");
-    
-    return $command;
-}
-
-sub addFIRProcessing {
-    my ($baseCommand, $leftFIR, $rightFIR) = @_;
-    
-    return $baseCommand unless ($leftFIR || $rightFIR);
-    
-    my $soxPath = $prefs->get('sox_path') || '/usr/bin/sox';
-    
-    my $firCommand = " | $soxPath - -t wav -";
-    
-    if ($leftFIR && $rightFIR) {
-        $firCommand .= " remix 1 2";
-        $firCommand .= " fir \"$leftFIR\" \"$rightFIR\"";
-    } elsif ($leftFIR) {
-        $firCommand .= " remix 1 1";
-        $firCommand .= " fir \"$leftFIR\"";
-    } elsif ($rightFIR) {
-        $firCommand .= " remix 2 2";
-        $firCommand .= " fir \"$rightFIR\"";
-    }
-    
-    return $baseCommand . $firCommand;
 }
 
 1;
