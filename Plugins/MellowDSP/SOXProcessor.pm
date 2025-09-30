2342fe08991cb053580c2329c02ceee606495fec
mellowsoftwarehouse <mellow@softwarehouse.com>
2025-09-30 10:52:12 +0200

    MellowDSP v2.1.1 - Add output format selection
    - WAV (uncompressed PCM) or FLAC (lossless compressed) output
    - FLAC uses compression level 0 for minimal CPU
    - Updated PlayerSettings interface and handler
    - SOXProcessor builds commands with selected format
    - User can choose based on network bandwidth needs

--- Plugins/MellowDSP/SOXProcessor.pm
+++ Plugins/MellowDSP/SOXProcessor.pm
@@ -13,7 +13,7 @@ sub init {
 }
 
 sub buildTranscodeCommand {
-    my ($client, $inputFormat, $outputFormat, $inputFile, $outputFile) = @_;
+    my ($class, $client, $inputFormat, $outputFormat, $inputFile, $outputFile) = @_;
     
     return '' unless $client;
     
@@ -25,21 +25,24 @@ sub buildTranscodeCommand {
     
     my $targetRate = $clientPrefs->get('target_rate') || '176400';
     my $phaseResponse = $clientPrefs->get('phase_response') || 'linear';
+    my $outputDepth = $clientPrefs->get('output_depth') || '24';
+    my $outputFmt = $clientPrefs->get('output_format') || 'wav';
     my $bufferSize = $prefs->get('buffer_size') || 8;
     
-    # Costruisci comando SOX
     my @cmd = ($soxPath);
     
-    # Input
     push @cmd, '-t', $inputFormat if $inputFormat;
     push @cmd, $inputFile;
     
-    # Output
-    push @cmd, '-t', 'wav';
-    push @cmd, '-b', '24';
+    push @cmd, '-t', $outputFmt;
+    push @cmd, '-b', $outputDepth;
+    
+    if ($outputFmt eq 'flac') {
+        push @cmd, '-C', '0';
+    }
+    
     push @cmd, $outputFile;
     
-    # Resampling con fase specifica
     if ($targetRate && $targetRate ne '44100') {
         push @cmd, 'rate', '-v';
         
@@ -48,13 +51,12 @@ sub buildTranscodeCommand {
         } elsif ($phaseResponse eq 'intermediate') {
             push @cmd, '-i';
         } else {
-            push @cmd, '-s'; # linear phase (default high quality)
+            push @cmd, '-s';
         }
         
         push @cmd, $targetRate;
     }
     
-    # Buffer size
     push @cmd, 'buffer', $bufferSize * 1024;
     
     my $command = join(' ', map { /\s/ ? "\"$_\"" : $_ } @cmd);
@@ -62,28 +64,6 @@ sub buildTranscodeCommand {
     $log->info("SOX command for " . $client->name() . ": $command");
     
     return $command;
-}
-
-sub getSupportedFormats {
-    my ($client) = @_;
-    
-    return [] unless $client;
-    
-    my $clientPrefs = $prefs->client($client);
-    my $inputFormats = $clientPrefs->get('input_formats') || 'flac,wav';
-    
-    return [split(',', $inputFormats)];
-}
-
-sub getOutputRates {
-    my ($client) = @_;
-    
-    return [] unless $client;
-    
-    my $clientPrefs = $prefs->client($client);
-    my $outputRates = $clientPrefs->get('output_rates') || '44100,48000,88200,96000,176400,192000,352800,384000,705600,768000';
-    
-    return [split(',', $outputRates)];
 }
 
 1;
