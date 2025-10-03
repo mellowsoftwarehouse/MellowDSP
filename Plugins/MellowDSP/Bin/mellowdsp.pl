b7d28907445f3d85f833fa59c53f8f1736bd217f
mellowsoftwarehouse <mellow@softwarehouse.com>
2025-10-03 16:25:25 +0200

    v3.0.5: Rebuild clean package with correct install.xml and Bin/mellowdsp.pl

--- Plugins/MellowDSP/Bin/mellowdsp.pl
+++ Plugins/MellowDSP/Bin/mellowdsp.pl
@@ -2,11 +2,10 @@
 use strict;
 use warnings;
 use Getopt::Long;
-use File::Spec::Functions qw(catfile);
 
 my $logfile = '/tmp/mellowdsp_wrapper.log';
 open(my $log, '>>', $logfile) or die "Cannot open log: $!";
-print $log "\n=== MellowDSP Wrapper v3.1.0 Called ===\n";
+print $log "\n=== MellowDSP Wrapper v3.0.5 Called ===\n";
 print $log "Time: " . localtime() . "\n";
 print $log "Raw ARGV: " . join(' ', @ARGV) . "\n";
 
@@ -18,9 +17,6 @@ GetOptions(
     's=s' => \$options->{startSec},
     'w=s' => \$options->{durationSec},
     'r=s' => \$options->{samplerate},
-    'fir'  => \$options->{firEnabled},
-    'fir-left=s' => \$options->{firLeft},
-    'fir-right=s' => \$options->{firRight},
 );
 
 my $file = shift @ARGV;
@@ -28,9 +24,7 @@ my $file = shift @ARGV;
 print $log "Parsed options:\n";
 print $log "  clientId: " . ($options->{clientId} || 'NONE') . "\n";
 print $log "  inCodec: " . ($options->{inCodec} || 'NONE') . "\n";
-print $log "  outCodec: " . ($options->{outCodec} || 'NONE') . "\n";
 print $log "  samplerate: " . ($options->{samplerate} || 'NONE') . "\n";
-print $log "  firEnabled: " . ($options->{firEnabled} ? 'YES' : 'NO') . "\n";
 print $log "  file: " . ($file || 'NONE') . "\n";
 
 if (!$file || $file eq '') {
@@ -52,60 +46,22 @@ my %codecMap = (
 );
 
 my $inFormat = $codecMap{$options->{inCodec}} || $options->{inCodec} || 'flac';
-my $outFormat = $options->{outCodec} || 'wav';
 my $soxPath = '/usr/bin/sox';
 
-my @soxCmd = ($soxPath, '-t', $inFormat, $file, '-t', 'wav', '-b', '24', '-');
+my @cmd = ($soxPath, '-t', $inFormat, $file, '-t', 'wav', '-b', '24', '-');
 
 if ($options->{samplerate} && $options->{samplerate} > 0) {
-    push @soxCmd, 'rate', '-v', '-s', $options->{samplerate};
+    push @cmd, 'rate', '-v', '-s', $options->{samplerate};
 }
 
 if ($options->{startSec}) {
-    push @soxCmd, 'trim', $options->{startSec};
+    push @cmd, 'trim', $options->{startSec};
     if ($options->{durationSec}) {
-        push @soxCmd, $options->{durationSec};
+        push @cmd, $options->{durationSec};
     }
 }
 
-if ($options->{firEnabled} && $options->{firLeft} && $options->{firRight}) {
-    print $log "FIR filtering enabled\n";
-    print $log "  Left: " . $options->{firLeft} . "\n";
-    print $log "  Right: " . $options->{firRight} . "\n";
-    
-    my $pluginDir = $ENV{MELLOWDSP_PLUGIN_DIR} || '/var/daphile/mediaserver/cache/InstalledPlugins/Plugins/MellowDSP';
-    my $camillaPath = catfile($pluginDir, 'Bin', 'camilladsp');
-    my $configPath = catfile($pluginDir, 'Configs', $options->{clientId} . '.yml');
-    
-    print $log "CamillaDSP path: $camillaPath\n";
-    print $log "Config path: $configPath\n";
-    
-    if (!-x $camillaPath) {
-        print $log "ERROR: CamillaDSP not found or not executable\n";
-        close($log);
-        die "CamillaDSP not available\n";
-    }
-    
-    print $log "Pipeline: SOX → CamillaDSP → output\n";
-    print $log "SOX command: " . join(' ', @soxCmd) . "\n";
-    
-    open(my $sox, '-|', @soxCmd) or die "Cannot start SOX: $!";
-    open(my $camilla, '|-', $camillaPath, $configPath) or die "Cannot start CamillaDSP: $!";
-    
-    while (read($sox, my $buffer, 65536)) {
-        print $camilla $buffer;
-    }
-    
-    close($sox);
-    close($camilla);
-    
-    print $log "Pipeline completed\n";
-    close($log);
-    exit 0;
-}
-
-print $log "Direct SOX processing (no FIR)\n";
-print $log "Final SOX command: " . join(' ', @soxCmd) . "\n";
+print $log "Final SOX command: " . join(' ', @cmd) . "\n";
 close($log);
 
-exec @soxCmd or die "Failed to exec: $!";
+exec @cmd or die "Failed to exec: $!";
