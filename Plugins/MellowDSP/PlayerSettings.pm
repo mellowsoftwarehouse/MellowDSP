8bdc1082c25a9e82225c8182cb6158b63374dbf5
mellowsoftwarehouse <mellow@softwarehouse.com>
2025-10-03 13:53:57 +0200

    v3.1.1: Fix PlayerSettings.pm - align prefs with HTML template to restore player menu

--- Plugins/MellowDSP/PlayerSettings.pm
+++ Plugins/MellowDSP/PlayerSettings.pm
@@ -21,48 +21,21 @@ sub page {
 sub prefs {
     my ($class, $client) = @_;
     return ($prefs->client($client), qw(
-        enabled
-        output_format
-        output_samplerate
+        player_enabled
+        input_aiff
+        input_alac
+        input_flac
+        input_wav
+        target_rate
         phase_response
+        output_depth
+        dither_type
+        dither_precision
         fir_enabled
         fir_left
         fir_right
+        output_format
     ));
-}
-
-sub handler {
-    my ($class, $client, $params) = @_;
-    
-    if ($params->{'saveSettings'}) {
-    }
-    
-    $params->{output_formats} = [
-        { value => 'wav',  label => 'WAV (Uncompressed)' },
-        { value => 'flac', label => 'FLAC (Lossless)' },
-    ];
-    
-    $params->{sample_rates} = [
-        { value => '0',      label => 'No resampling' },
-        { value => '44100',  label => '44.1 kHz' },
-        { value => '48000',  label => '48 kHz' },
-        { value => '88200',  label => '88.2 kHz' },
-        { value => '96000',  label => '96 kHz' },
-        { value => '176400', label => '176.4 kHz' },
-        { value => '192000', label => '192 kHz' },
-        { value => '352800', label => '352.8 kHz' },
-        { value => '384000', label => '384 kHz' },
-        { value => '705600', label => '705.6 kHz' },
-        { value => '768000', label => '768 kHz' },
-    ];
-    
-    $params->{phase_responses} = [
-        { value => 'linear',       label => 'Linear Phase' },
-        { value => 'intermediate', label => 'Intermediate Phase' },
-        { value => 'minimum',      label => 'Minimum Phase' },
-    ];
-    
-    return $class->SUPER::handler($client, $params);
 }
 
 1;
